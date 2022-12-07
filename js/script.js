967f88b3dac3e114f8fa476a0ee6e98d4d7ecaa8
Chaooohs <01nikka01@gmail.com>
2022-12-07 12:24:43 +0200

    added no-scroll

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 const nav = document.querySelector('#nav');
 const burger = document.querySelector('#burger');
 const burgerImage = document.querySelector('#burger-img');
+const body = document.querySelector('body');                //! выборка элемента
 
 burger.onclick = () => {
-  if (nav.classList.toggle('active')) {
+  if (nav.classList.toggle('active')) {                 //! переключает класс на селекторе nav
     burgerImage.src = './img/header/close-btn.svg';
+    body.classList.add('no-scroll');                    //! добавляет класс к селектору body
   } else {
     burgerImage.src = './img/header/open-btn.svg';
+    body.classList.remove('no-scroll');                  //! удаляет класс с body
   }
 }
